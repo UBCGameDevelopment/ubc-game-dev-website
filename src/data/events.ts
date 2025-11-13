d25783f4dbe1547530d4a2a1536e405f05553617
vvictort <vvictort20@gmail.com>
2025-11-12 23:41:50 -0800

    added pictures to replace placeholders

--- src/data/events.ts
+++ src/data/events.ts
@@ -3,6 +3,8 @@ import type { ImageMetadata } from "astro";
 
 // Images
 import placeholderImage from "../assets/logo.png";
+import launchPartyImage from "../assets/timeline/launch_party.jpg";
+import unityImage from "../assets/timeline/unity.png";
 
 export type Event = {
   title: string;
@@ -19,21 +21,23 @@ export const events: Event[] = [
     date: "Friday, Sept. 26, 2025: 6-9pm",
     description: "Our launch party is the perfect chance to learn more about what exactly the club does. At our launch party, you will hear about how you can join a team, what our events look like and what ways the club can help support you! Its your chance to become a member.",
     location: "BUCH A104",
-    mapLink: "https://maps.app.goo.gl/yyXBtJi8rwkoMd487",
-    image: placeholderImage,
+    mapLink: "https://maps.app.goo.gl/kXj9mxqmohCmDsbt7",
+    image: launchPartyImage,
   },
   {
-    title: "Game Jam Workshop",
-    date: "Saturday, Oct. 12, 2025: 10am-4pm",
-    description: "Join us for an intensive game development workshop where you'll learn the basics of game design, coding, and art. Work in teams to create a complete game in just one day!",
-    location: "ICICS X836",
-    image: placeholderImage,
+    title: "Alan's Unity Workshop",
+    date: "Wed., Nov. 19, 2025: 6:30-8pm",
+    description: "Join us for an intensive game development workshop where you'll learn the basics of Unity. Get the required skills to create your own games with us!",
+    location: "BUCH D222",
+    mapLink: "https://maps.app.goo.gl/5s75UNe32sQprvFJA",
+    image: unityImage,
   },
   {
-    title: "Industry Speaker Series",
-    date: "Wednesday, Nov. 6, 2025: 5-7pm",
-    description: "Hear from industry professionals about their journey in game development. Network with developers from local studios and learn about career opportunities in the gaming industry.",
-    location: "ESB 1012",
+    title: "Battle of the Concepts",
+    date: "Friday, Nov. 21, 2025: 6-8pm",
+    description: "Step into the arena of creativity! Members pitch their boldest game ideas, showcase their vision, and compete for the club’s vote. The concept that wins the crowd could become our next big game project — who will rise as the ultimate creator?",
+    location: "BUCH A104",
+    mapLink: "https://maps.app.goo.gl/kXj9mxqmohCmDsbt7",
     image: placeholderImage,
   },
 ];
