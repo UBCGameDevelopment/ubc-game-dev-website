f60756a1b106e21324ac7e1c2b7f0682fc570f10
vvictort <vvictort20@gmail.com>
2025-11-24 22:48:08 -0800

    fixed footer to use icon components

---
+++ src/config/links.ts
@@ -0,0 +1,24 @@
+/**
+ * Centralized configuration for external links used throughout the site
+ * Update these URLs in one place to reflect changes across the entire application
+ */
+
+export const SOCIAL_LINKS = {
+  discord: "https://discord.gg/RXMxvEcj7B",
+  instagram: "https://www.instagram.com/ubcgamedev/",
+  twitch: "https://www.twitch.tv/ubcgamedev",
+  // Add other social links as needed
+  // twitter: "https://twitter.com/ubcgamedev",
+  // facebook: "https://facebook.com/ubcgamedev",
+} as const;
+
+export const EXTERNAL_LINKS = {
+  // Add other external links as needed
+  // github: "https://github.com/UBCGameDevelopment",
+} as const;
+
+// Export all links for convenience
+export const LINKS = {
+  ...SOCIAL_LINKS,
+  ...EXTERNAL_LINKS,
+} as const;
